79c750859652f4c78a7223b27338238740cc3a57
Ky Nguyen <cuty0406@outlook.com>
2025-05-27 14:30:00 +0700

    feat: add api gateway

---
+++ api-gateway/src/test/java/com/trikynguci/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trikynguci.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
